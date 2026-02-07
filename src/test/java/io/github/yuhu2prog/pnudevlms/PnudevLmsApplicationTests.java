297a1d470550bb72545195e86254d45209b61650
Arsenii Holovanov <arsenijgolovanev@gmail.com>
2026-02-07 11:22:16 +0200

    Initialized basic spring-boot project

---
+++ src/test/java/io/github/yuhu2prog/pnudevlms/PnudevLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yuhu2prog.pnudevlms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnudevLmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
